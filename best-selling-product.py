30f7376becbff4e306e0cc957eb09dcac74d1d0f
Muhammad1382 <112013891+Muhammad1382@users.noreply.github.com>
2025-10-29 12:16:40 +0330

    Add files via upload

---
+++ best-selling-product.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df =  pd.read_csv (r"C:\Users\Ertebat\Desktop\project python\Sales data.csv")
+
+product_counts = df['Product'].value_counts()
+
+
+plt.figure(figsize=(15,6))
+ax = sns.barplot(x=product_counts.index, y=product_counts.values)
+ax.bar_label(container=ax.containers[0],label_type='edge')
+
+
+
+plt.xticks(rotation=45, ha='right')  # برچسب‌ها خواناتر باشن
+plt.ylabel("Number of Sales")
+plt.xlabel("Product")
+plt.title("Products Sold")
+plt.tight_layout()
+plt.show()
